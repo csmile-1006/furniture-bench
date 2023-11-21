c71752fdd7298009e1e0a763e58463e1ccb5815b
csmile-1006 <changyeon.kim@kaist.ac.kr>
2023-11-21 19:36:19 +0900

    change extract_feature with new ARP-V2 arch

--- implicit_q_learning/extract_feature_with_reward.py
+++ implicit_q_learning/extract_feature_with_reward.py
@@ -1,12 +1,15 @@
+import sys
 import pickle
 from pathlib import Path
 
 import numpy as np
 import torch
-import gym
+import torchvision.transforms as T
 from absl import app, flags
 from ml_collections import ConfigDict
 
+sys.path.append("/home/changyeon/ICML2024/BPref-v2/")
+from bpref_v2.data.label_reward_furniturebench import load_reward_model, load_reward_fn  # noqa: E402
 
 FLAGS = flags.FLAGS
 
@@ -17,21 +20,45 @@ flags.DEFINE_boolean("use_r3m", False, "Use r3m to encode images.")
 flags.DEFINE_boolean("use_vip", False, "Use vip to encode images.")
 flags.DEFINE_integer("num_threads", int(8), "Set number of threads of PyTorch")
 flags.DEFINE_integer("num_demos", None, "Number of demos to convert")
-flags.DEFINE_integer('batch_size', 512, 'Batch size for encoding images')
+flags.DEFINE_integer("batch_size", 512, "Batch size for encoding images")
 flags.DEFINE_string("ckpt_path", "", "ckpt path of reward model.")
 flags.DEFINE_string("rm_type", "ARP-V2", "reward model type.")
+flags.DEFINE_string("pvr_type", "liv", "pvr type.")
 
 
-import sys
-sys.path.append("/home/changyeon/ICML2024/BPref-v2/")
-from bpref_v2.data.label_reward_furniturebench import load_reward_model, load_reward_fn
+device = torch.device("cuda")
+
+
+def load_embedding(rep="vip"):
+    if rep == "vip":
+        from vip import load_vip
+
+        model = load_vip()
+        transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor()])
+        feature_dim = 1024
+    if rep == "r3m":
+        from r3m import load_r3m
+
+        model = load_r3m("resnet50")
+        transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor()])
+        feature_dim = 2048
+    if rep == "liv":
+        from liv import load_liv
+
+        model = load_liv()
+        transform = T.Compose([T.ToTensor()])
+        feature_dim = 1024
+    model.eval()
+    model = model.module.to(device)
+    return model, transform, feature_dim
+
 
 def main(_):
     if FLAGS.num_threads > 0:
         print(f"Setting torch.num_threads to {FLAGS.num_threads}")
         torch.set_num_threads(FLAGS.num_threads)
 
-    env_type = 'Image'
+    env_type = "Image"
     furniture = FLAGS.furniture
     demo_dir = FLAGS.demo_dir
 
@@ -39,6 +66,7 @@ def main(_):
     ckpt_path = Path(FLAGS.ckpt_path).expanduser()
     reward_model = load_reward_model(rm_type=FLAGS.rm_type, ckpt_path=ckpt_path)
     reward_fn = load_reward_fn(rm_type=FLAGS.rm_type, reward_model=reward_model)
+    pvr_model, pvr_transform, feature_dim = load_embedding(rep=FLAGS.pvr_type)
 
     dir_path = Path(demo_dir)
 
@@ -53,21 +81,23 @@ def main(_):
     if FLAGS.use_r3m:
         # Use R3M for the image encoder.
         from r3m import load_r3m
-        encoder = load_r3m('resnet50')
+
+        encoder = load_r3m("resnet50")
 
     if FLAGS.use_vip:
         # Use VIP for the image encoder.
         from vip import load_vip
+
         encoder = load_vip()
 
     if FLAGS.use_r3m or FLAGS.use_vip:
         encoder.eval()
         for param in encoder.parameters():
             param.requires_grad = False
-        encoder.to('cuda')
+        encoder.to("cuda")
         device = torch.device("cuda")
 
-    files = list(dir_path.glob('2023-*.pkl'))
+    files = list(dir_path.glob("2023-*.pkl"))
     len_files = len(files)
 
     if len_files == 0:
@@ -80,29 +110,39 @@ def main(_):
         with open(file_path, "rb") as f:
             x = pickle.load(f)
 
-            if len(x['observations']) == len(x['actions']):
+            if len(x["observations"]) == len(x["actions"]):
                 # Dummy
-                x['observations'].append(x['observations'][-1])
-            l = len(x["observations"])
+                x["observations"].append(x["observations"][-1])
+            length = len(x["observations"])
 
             if FLAGS.use_r3m or FLAGS.use_vip:
-                img1 = [x["observations"][i]["color_image1"] for i in range(l)]
-                img2 = [x["observations"][i]["color_image2"] for i in range(l)]
+                img1 = [x["observations"][i]["color_image1"] for i in range(length)]
+                img2 = [x["observations"][i]["color_image2"] for i in range(length)]
                 img1 = torch.from_numpy(np.stack(img1))
                 img2 = torch.from_numpy(np.stack(img2))
 
                 if FLAGS.use_r3m:
-                    img1_feature = np.zeros((l, 2048), dtype=np.float32)
-                    img2_feature = np.zeros((l, 2048), dtype=np.float32)
+                    img1_feature = np.zeros((length, 2048), dtype=np.float32)
+                    img2_feature = np.zeros((length, 2048), dtype=np.float32)
                 elif FLAGS.use_vip:
-                    img1_feature = np.zeros((l, 1024), dtype=np.float32)
-                    img2_feature = np.zeros((l, 1024), dtype=np.float32)
+                    img1_feature = np.zeros((length, 1024), dtype=np.float32)
+                    img2_feature = np.zeros((length, 1024), dtype=np.float32)
 
                 with torch.no_grad():
                     # Use batch size.
-                    for i in range(0, l, FLAGS.batch_size):
-                        img1_feature[i:i+FLAGS.batch_size] = encoder(img1[i:i+FLAGS.batch_size].to(device).reshape(-1, 3, 224, 224)).cpu().detach().numpy()
-                        img2_feature[i:i+FLAGS.batch_size] = encoder(img2[i:i+FLAGS.batch_size].to(device).reshape(-1, 3, 224, 224)).cpu().detach().numpy()
+                    for i in range(0, length, FLAGS.batch_size):
+                        img1_feature[i : i + FLAGS.batch_size] = (
+                            encoder(img1[i : i + FLAGS.batch_size].to(device).reshape(-1, 3, 224, 224))
+                            .cpu()
+                            .detach()
+                            .numpy()
+                        )
+                        img2_feature[i : i + FLAGS.batch_size] = (
+                            encoder(img2[i : i + FLAGS.batch_size].to(device).reshape(-1, 3, 224, 224))
+                            .cpu()
+                            .detach()
+                            .numpy()
+                        )
 
             images = {key: val for key, val in [("color_image2", img2), ("color_image1", img1)]}
             for key, val in images.items():
@@ -118,39 +158,58 @@ def main(_):
             args.skip_frame = 16
             args.return_images = True
 
-            rewards, (_, stacked_attn_masks, stacked_timesteps) = reward_fn(images=images, actions=actions, skills=skills, args=args)
+            rewards, (_, stacked_attn_masks, stacked_timesteps) = reward_fn(
+                images=images,
+                actions=actions,
+                skills=skills,
+                args=args,
+                pvr_model=pvr_model,
+                pvr_transform=pvr_transform,
+                model_type=FLAGS.pvr_type,
+                feature_dim=feature_dim,
+                texts=None,
+                device=device,
+            )
             cumsum_skills = np.cumsum(x["skills"])
 
-            for i in range(l - 1):
+            for i in range(length - 1):
                 if FLAGS.use_r3m or FLAGS.use_vip:
                     image1 = img1_feature[stacked_timesteps[i]]
-                    next_image1 = img1_feature[stacked_timesteps[min(i + 1, l - 2)]]
+                    next_image1 = img1_feature[stacked_timesteps[min(i + 1, length - 2)]]
                     image2 = img2_feature[stacked_timesteps[i]]
-                    next_image2 = img2_feature[stacked_timesteps[min(i + 1, l - 2)]]
+                    next_image2 = img2_feature[stacked_timesteps[min(i + 1, length - 2)]]
                 else:
                     raise ValueError("You have to choose either use_r3m or use_vip.")
 
-                obs_.append({
-                    # 'image_feature': feature1,
-                    'image1': image1,
-                    'image2': image2,
-                    'robot_state': np.stack([x["observations"][idx]['robot_state'] for idx in stacked_timesteps[i]])
-                })
-                next_obs_.append({
-                    # 'image_feature': next_feature1,
-                    'image1': next_image1,
-                    'image2': next_image2,
-                    'robot_state': np.stack([x["observations"][idx]['robot_state'] for idx in stacked_timesteps[min(i + 1, l - 2)]])
-                })
+                obs_.append(
+                    {
+                        # 'image_feature': feature1,
+                        "image1": image1,
+                        "image2": image2,
+                        "robot_state": np.stack(
+                            [x["observations"][idx]["robot_state"] for idx in stacked_timesteps[i]]
+                        ),
+                    }
+                )
+                next_obs_.append(
+                    {
+                        # 'image_feature': next_feature1,
+                        "image1": next_image1,
+                        "image2": next_image2,
+                        "robot_state": np.stack(
+                            [x["observations"][idx]["robot_state"] for idx in stacked_timesteps[min(i + 1, length - 2)]]
+                        ),
+                    }
+                )
 
                 action_.append(x["actions"][i])
                 reward_.append(x["rewards"][i])
-                if i == l - 2:
+                if i == length - 2:
                     step_reward_.append(cumsum_skills[i] + 1)
                 else:
                     step_reward_.append(cumsum_skills[i])
                 multimodal_reward_.append(rewards[i])
-                done_.append(1 if i == l - 2 else 0)
+                done_.append(1 if i == length - 2 else 0)
 
     dataset = {
         "observations": obs_,
@@ -162,7 +221,7 @@ def main(_):
         "terminals": np.array(done_),
     }
 
-    path = f'data/{env_type}/{furniture}.pkl' if FLAGS.out_file_path is None else FLAGS.out_file_path
+    path = f"data/{env_type}/{furniture}.pkl" if FLAGS.out_file_path is None else FLAGS.out_file_path
     path = Path(path)
     path.parent.mkdir(exist_ok=True, parents=True)
 
