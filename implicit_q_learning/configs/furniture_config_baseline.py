06e4337f4013265c1f999c2f583cfd396bc85820
csmile-1006 <doli1573@kaist.ac.kr>
2024-02-06 01:17:23 +0900

    add baseline config

---
+++ implicit_q_learning/configs/furniture_config_baseline.py
@@ -0,0 +1,26 @@
+import ml_collections
+
+
+def get_config():
+    config = ml_collections.ConfigDict()
+
+    config.actor_lr = 1e-4
+    config.value_lr = 1e-4
+    config.critic_lr = 1e-4
+
+    config.hidden_dims = (512, 256, 256)
+
+    config.discount = 0.996
+
+    config.expectile = 0.8  # The actual tau for expectiles.
+    config.temperature = 0.5
+    config.dropout_rate = 0.0
+
+    config.tau = 0.005  # For soft target updates.
+
+    # transformer setup
+    config.emb_dim = 256
+    config.depth = 6
+    config.num_heads = 8
+
+    return config
